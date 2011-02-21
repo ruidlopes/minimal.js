509f6130c7d79752b89ded7c49a9794514258161
Rui Lopes <ruidlopes@gmail.com>
2011-02-21 20:12:44 +0000

    removed unneeded window creation in jsdom

--- minimal-node.js
+++ minimal-node.js
@@ -2,8 +2,7 @@ var jsdom = require("jsdom").jsdom;
 	
 exports.template = function(template) {
 	document = jsdom(template);
-	window   = document.createWindow();
-		
+	
 	var minimal = require("./minimal");
 
 	minimal.$m.html = function() {
